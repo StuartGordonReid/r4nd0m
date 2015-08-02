dc130fe9a4c88cdcd40336fcb80ca9a4bd0f7f88
Stuart <stuartgordonreid@gmail.com>
2015-08-02 16:48:14 +0200

    Found an unbiased binary flip

--- BinaryFrame.py
+++ BinaryFrame.py
@@ -30,3 +30,31 @@ class BinaryFrame:
                 else:
                     cbin_data += '0'
             self.bin_data[c] = cbin_data
+
+
+if __name__ == '__main__':
+    f, sum_one, sum_zero = -1.0, 0, 0
+    bit_count = numpy.zeros(31)
+    for i in range(21):
+        if f != 0.0:
+            bin_r = bitstring.BitArray(float=f, length=32)
+            bstring = str(bin_r._getbin())
+
+            if bstring[0] == '1':
+                bstring = bstring[1::]
+                bstring = bstring.replace('1', '2').replace('0', '1').replace('2', '0')
+                bstring = '1' + bstring
+
+            z = bstring.count("0")
+            o = bstring.count("1")
+            sum_one += o
+            sum_zero += z
+
+            for j in range(31):
+                bit_count[j] += int(bstring[j])
+
+            print(f, "\t", bstring, z, o)
+        f = round(f + 0.1, 1)
+
+    print(sum_one, sum_zero, sum_zero-sum_one)
+    print(bit_count)
